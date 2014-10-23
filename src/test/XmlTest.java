146ffa8ab22ff8d1dacd3df787709d7c5ebd12c4
yu <spyu2000@126.com>
2014-10-24 07:14:55 +0800

    test

---
+++ src/test/XmlTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class XmlTest {
+	
+	public static void main(String[] args){
+		
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); 
+			DocumentBuilder builder=factory.newDocumentBuilder(); 
+			Document doc=builder.parse("textxml.xml"); 
+//			doc.normalize(); 
+			
+			TransformerFactory tFactory =TransformerFactory.newInstance(); 
+			Transformer transformer = tFactory.newTransformer(); 
+			DOMSource source = new DOMSource(doc); 
+			StreamResult result = new StreamResult(new File("textxml1.xml")); 
+			transformer.transform(source, result);
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- src/test/httpclient/SimpleHttpClient.java
+++ src/test/httpclient/SimpleHttpClient.java
@@ -4,26 +4,82 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.HttpVersion;
+
+
+
 import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HeaderElementIterator;
+import org.apache.http.HeaderIterator;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
+import org.apache.http.ProtocolVersion;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.params.ClientPNames;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.http.message.BasicHeaderElementIterator;
 import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.util.EntityUtils;
 
 public class SimpleHttpClient {
+	
+	/**
+	 * 适合多线程的HttpClient,用httpClient4.2.1实现
+	 * @return DefaultHttpClient
+	 */
+	public static DefaultHttpClient getHttpClient()
+	{  		
+	    // 设置组件参数, HTTP协议的版本,1.1/1.0/0.9 
+		HttpParams params = new BasicHttpParams(); 
+	    HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1); 
+	    HttpProtocolParams.setUserAgent(params, "HttpComponents/1.1"); 
+	    HttpProtocolParams.setUseExpectContinue(params, true); 	  
+
+	    //设置连接超时时间 
+	    int REQUEST_TIMEOUT = 10*1000;	//设置请求超时10秒钟 
+		int SO_TIMEOUT = 10*1000; 		//设置等待数据超时时间10秒钟 
+		long CONN_MANAGER_TIMEOUT=500L;//获取连接超时时间
+	    params.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, REQUEST_TIMEOUT);  
+	    params.setParameter(CoreConnectionPNames.SO_TIMEOUT, SO_TIMEOUT); 
+	    params.setLongParameter(ClientPNames.CONN_MANAGER_TIMEOUT, CONN_MANAGER_TIMEOUT);
+	    params.setBooleanParameter(CoreConnectionPNames.STALE_CONNECTION_CHECK, true);
+	    
+		//设置访问协议 
+		SchemeRegistry schreg = new SchemeRegistry();  
+		schreg.register(new Scheme("http",80,PlainSocketFactory.getSocketFactory())); 
+		schreg.register(new Scheme("https", 443, SSLSocketFactory.getSocketFactory())); 	  
+		
+		//多连接的线程安全的管理器 
+		PoolingClientConnectionManager pccm = new PoolingClientConnectionManager(schreg);
+		pccm.setDefaultMaxPerRoute(100);	//每个主机的最大并行链接数 
+		pccm.setMaxTotal(100);			//客户端总并行链接最大数    
+		
+		DefaultHttpClient httpClient = new DefaultHttpClient(pccm, params);  
+		return httpClient;
+	}
 
 	public static void simpleHttpClient() {
 
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		HttpGet httpGet = new HttpGet("http://www.baidu.com");
-		HttpResponse response = null;
+		CloseableHttpResponse  response = null;
 		try {
 			response = httpClient.execute(httpGet);
 			HttpEntity entity = response.getEntity();
@@ -32,15 +88,17 @@ public class SimpleHttpClient {
 				System.out.println(header.toString());
 			}
 			System.out.println(EntityUtils.toString(entity));
+			EntityUtils.consume(entity);
 		} catch (ClientProtocolException e) {
-
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
+			
 			try {
+				response.close();
 				httpGet.releaseConnection();
 				httpClient.close();
 			} catch (IOException e) {
@@ -71,23 +129,57 @@ public class SimpleHttpClient {
 		}
 
 	}
-	public static void simpleHttpResponse(){
-		HttpResponse response=new BasicHttpResponse(HttpVersion.HTTP_1_1, HttpStatus.SC_OK,"OK");
 
+	public static void simpleHttpResponse() {
+		HttpResponse response = new BasicHttpResponse(HttpVersion.HTTP_1_1,
+				HttpStatus.SC_OK, "OK");
 
-				  
+		System.out.println(response.getProtocolVersion());
 
-				System.out.println(response.getProtocolVersion());  
+		System.out.println(response.getStatusLine().getStatusCode());
 
-				System.out.println(response.getStatusLine().getStatusCode());  
+		System.out.println(response.getStatusLine().getReasonPhrase());
 
-				System.out.println(response.getStatusLine().getReasonPhrase());  
+		System.out.println(response.getStatusLine().toString());
+	}
 
-				System.out.println(response.getStatusLine().toString());  
-				}
+	public static void simpleHttpResponseHead() {
+		HttpResponse response = new BasicHttpResponse(
+				org.apache.http.HttpVersion.HTTP_1_1,
+				org.apache.http.HttpStatus.SC_OK, "OK");
+		response.addHeader("Set-Cookie","c1=a;path=/;domain=localhost");
+		response.addHeader("Set-Cookie","c2=b;path=\"/\";domain=\"localhost\"");
+		
+		Header header1=response.getFirstHeader("Set-Cookie");
+		Header header2=response.getLastHeader("Set-Cookie");
+		
+		Header[] heads=response.getHeaders("Set-Cookie");
+		
+		System.out.println(heads.length);
+		
+		HeaderIterator itr=response.headerIterator("Set-Cookie");
+		HeaderElementIterator itr1=new BasicHeaderElementIterator(response.headerIterator("Set-Cookie"));
+		
+		HeaderElement element=null;
+		while(itr1.hasNext()){
+			element= itr1.nextElement();
+			System.out.println(element.getName()+"    "+element.getValue());
+			
+			NameValuePair[] params=element.getParameters();
+			for (int i = 0; i < params.length; i++) {  
+
+		        System.out.println(" " + params[i]);  
+
+		    }  
+			
+		}
+	}
 
 	public static void main(String[] args) {
 		SimpleHttpClient.simpleHttpClient();
 		SimpleHttpClient.simpleHttpClientUrl();
+		SimpleHttpClient.simpleHttpResponse();
+		SimpleHttpClient.simpleHttpResponseHead();
+
 	}
 }
